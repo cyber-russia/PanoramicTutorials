2d9cbfe92c89b62f4b8e613d0ca30d9cfba17dc5
Pachev Artem <pachevartem@ya.ru>
2018-07-02 11:33:26 +0300

    Complete Scene for Panoramic Tutorals

---
+++ Assets/Scripts/FreeCameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SupremumStudio
+{
+	/// <summary>
+	/// Компонент позволяющий вращать камеру вокруг своей оси
+	/// </summary>
+	public class FreeCameraController : MonoBehaviour
+	{
+		[Range(1,10)]
+		[Header("Чувствительность мышки")]
+		public float sensitivity = 1f;
+		[Range(0,80)]
+		[Header("Максимальный угол отклонения по оси Y")]		
+		public float maxYAngle = 80f;
+		
+		private Vector2 currentRotation;
+
+		void Update()
+		{
+			currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+			currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+
+			currentRotation.x = Mathf.Repeat(currentRotation.x, 360);  /// ...358-359-0-1-2...
+			currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle); // [-80,80]
+			
+			Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);	
+		}
+
+	}
+}
+
---
+++ Assets/Scripts/_testScripts.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _testScripts : MonoBehaviour {
+    private void Update()
+    {
+        transform.position = new Vector3(Mathf.Repeat(Time.time, 3), transform.position.y, transform.position.z);
+    }
+}
